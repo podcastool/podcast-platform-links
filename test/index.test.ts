e28ba3f851e06ec5e8f6ec6af7ce98bcddbb0ae5
Jeff Wen <3297859+sinchang@users.noreply.github.com>
2021-08-22 14:57:52 +0800

    feat: Update podcast platform name (#5)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,61 +1,61 @@
-import { getPodacstPlatformLinks } from '../src'
+import { getPodacstPlatformLinks, PodcastPlatformLinkItem } from '../src'
 
 describe('getPodacstPlatformLinks', () => {
   it('should work', async() => {
-    const expectResult = [
+    const expectResult: PodcastPlatformLinkItem[] = [
       {
-        platform: 'apple',
+        platform: 'Apple Podcasts',
         link: 'https://podcasts.apple.com/podcast/id1088178402',
       },
       {
-        platform: 'breaker',
+        platform: 'Breaker',
         link: 'https://breaker.audio/shows?feed_url=https%3A%2F%2Fcrazy.capital%2Ffeed',
       },
       {
-        platform: 'bullhorn',
+        platform: 'Bullhorn',
         link: 'https://bullhorn.fm/podchaser/itunes/1088178402',
       },
-      { platform: 'castbox', link: 'https://castbox.fm/vic/1088178402' },
-      { platform: 'castro', link: 'https://castro.fm/itunes/1088178402' },
+      { platform: 'Castbox', link: 'https://castbox.fm/vic/1088178402' },
+      { platform: 'Castro', link: 'https://castro.fm/itunes/1088178402' },
       {
-        platform: 'google',
+        platform: 'Google Podcasts',
         link: 'https://podcasts.google.com/?feed=aHR0cHM6Ly9jcmF6eS5jYXBpdGFsL2ZlZWQ=',
       },
-      { platform: 'overcast', link: 'https://overcast.fm/itunes1088178402' },
+      { platform: 'Overcast', link: 'https://overcast.fm/itunes1088178402' },
       {
-        platform: 'player',
+        platform: 'Player FM',
         link: 'https://player.fm/series/https%3A%2F%2Fcrazy.capital%2Ffeed',
       },
-      { platform: 'pocketcasts', link: 'https://pca.st/itunes/1088178402' },
+      { platform: 'Pocket Casts', link: 'https://pca.st/itunes/1088178402' },
       {
-        platform: 'podbean',
+        platform: 'Podbean',
         link: 'https://podbean.com/play/https%3A%2F%2Fcrazy.capital%2Ffeed',
       },
       {
-        platform: 'podcastaddict',
+        platform: 'Podcast Addict',
         link: 'https://podcastaddict.com/feed/https%3A%2F%2Fcrazy.capital%2Ffeed',
       },
       {
-        platform: 'podcastguru',
+        platform: 'Podcast Guru',
         link: 'https://app.podcastguru.io/podcast/1088178402',
       },
       {
-        platform: 'podfriend',
+        platform: 'Podfriend',
         link: 'https://web.podfriend.com/podcast/1088178402',
       },
       {
-        platform: 'podhero',
+        platform: 'Podhero',
         link: 'https://podhero.com/podcast/feed/https%3A%2F%2Fcrazy.capital%2Ffeed',
       },
       {
-        platform: 'podknife',
+        platform: 'Podknife',
         link: 'https://podknife.com/podcast?feed_url=1088178402',
       },
       {
-        platform: 'radiopublic',
+        platform: 'RadioPublic',
         link: 'https://radiopublic.com/https%3A%2F%2Fcrazy.capital%2Ffeed',
       },
-      { platform: 'sonnet', link: 'https://sonnet.fm/p/1088178402' },
+      { platform: 'Sonnet', link: 'https://sonnet.fm/p/1088178402' },
     ]
     const actualResult = await getPodacstPlatformLinks(1088178402)
     expect(actualResult).toEqual(expectResult)
