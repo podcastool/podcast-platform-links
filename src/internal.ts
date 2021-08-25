d2f1e262fbe0d74c408df659ead89150467f5863
Jeff Wen <3297859+sinchang@users.noreply.github.com>
2021-08-25 13:32:31 +0800

    feat!: return podcast basic meta (#6)

--- src/internal.ts
+++ src/internal.ts
@@ -1,13 +1,24 @@
 import got from 'got'
-import { PodcastLookupResult } from './types'
+import pick from 'lodash-es/pick'
+import { PodcastLookupResult, PodcastLookupResultItem } from './types'
 
 const API_URL = 'https://itunes.apple.com'
 
-export async function getFeedUrl(appleId: number): Promise<string> {
+export async function getPodcastInfo(appleId: number): Promise<PodcastLookupResultItem> {
+  const defaultInfo: PodcastLookupResultItem = {
+    feedUrl: '',
+    collectionId: 0,
+    collectionName: '',
+    collectionViewUrl: '',
+    artistName: '',
+    artworkUrl600: '',
+  }
+
   const data = await got(
     `${API_URL}/lookup?id=${appleId}`,
   ).json<PodcastLookupResult>()
-  return data.resultCount > 0 ? data.results[0].feedUrl : ''
+  // @ts-ignore
+  return data.resultCount > 0 ? pick(data.results[0], Object.keys(defaultInfo)) : defaultInfo
 }
 
 export const base64Encode = (str: string): string => {
--- test/internal.test.ts
+++ test/internal.test.ts
@@ -1,10 +1,19 @@
-import { getFeedUrl, base64Encode } from '../src/internal'
+import { getPodcastInfo, base64Encode } from '../src/internal'
 
-describe('getFeedUrl', () => {
+describe('getPodcastInfo', () => {
   it('should work', async() => {
-    const expectResult = 'https://crazy.capital/feed'
-    const actualResult = await getFeedUrl(1088178402)
-    expect(actualResult).toEqual(expectResult)
+    const expectResult = {
+      artistName: '黄海、Rio',
+      artworkUrl600:
+        'https://is3-ssl.mzstatic.com/image/thumb/Podcasts115/v4/e8/d8/8e/e8d88ed9-b12c-b8d9-5e5d-25682f72c182/mza_4966428153200416701.png/600x600bb.jpg',
+      collectionId: 1088178402,
+      collectionName: '疯投圈',
+      collectionViewUrl:
+        'https://podcasts.apple.com/us/podcast/%E7%96%AF%E6%8A%95%E5%9C%88/id1088178402?uo=4',
+      feedUrl: 'https://crazy.capital/feed',
+    }
+    const actualResult = await getPodcastInfo(1088178402)
+    expect(actualResult).toStrictEqual(expectResult)
   })
 })
 
